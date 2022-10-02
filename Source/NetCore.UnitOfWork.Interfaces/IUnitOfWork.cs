e89c850725ec7ae207a0f0c2e83e0b1e0f7001f2
Esaf <esaf3349@gmail.com>
2022-10-02 23:25:52 +0300

    Added repositories

--- Source/NetCore.UnitOfWork.Interfaces/IUnitOfWork.cs
+++ Source/NetCore.UnitOfWork.Interfaces/IUnitOfWork.cs
@@ -1,9 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using NetCore.UnitOfWork.Interfaces.Repositories;
+using System.Threading.Tasks;
 
 namespace NetCore.UnitOfWork.Interfaces
 {
     public interface IUnitOfWork
     {
+        IChatRepository Chats { get; }
+        IMessageRepository Messages { get; }
+        IPersonRepository Persons { get; }
+
         Task Commit();
         void Dispose();
     }
---
+++ Source/NetCore.UnitOfWork.Interfaces/Repositories/IChatRepository.cs
@@ -0,0 +1,10 @@
+﻿using NetCore.UnitOfWork.Core.Entities;
+using NetCore.UnitOfWork.Interfaces.Repositories.Common;
+
+namespace NetCore.UnitOfWork.Interfaces.Repositories
+{
+    public interface IChatRepository : IGenericRepository<Chat>
+    {
+
+    }
+}
