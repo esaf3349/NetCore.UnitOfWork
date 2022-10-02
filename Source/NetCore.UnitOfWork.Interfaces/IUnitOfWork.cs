a39b096a77389d3e17d93bf4a969fa5e1eb09254
Esaf <esaf3349@gmail.com>
2022-10-02 22:21:20 +0300

    Added Interfaces project

---
+++ Source/NetCore.UnitOfWork.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace NetCore.UnitOfWork.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task Commit();
+        void Dispose();
+    }
+}
---
+++ Source/NetCore.UnitOfWork.Interfaces/Repositories/Common/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace NetCore.UnitOfWork.Interfaces.Repositories.Common
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<bool> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(int id);
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> Get(Expression<Func<T, bool>> filter, int take, int offset);
+    }
+}
