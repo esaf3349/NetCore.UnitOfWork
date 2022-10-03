ff9d7b6163f86d7dddf9710d0e9e6bf9f3dbb977
Esaf <esaf3349@gmail.com>
2022-10-03 20:41:25 +0300

    Added repositories

---
+++ Source/NetCore.UnitOfWork.EntityFramework/Repositories/ChatRepository.cs
@@ -0,0 +1,14 @@
+﻿using NetCore.UnitOfWork.Core.Entities;
+using NetCore.UnitOfWork.EntityFramework.Repositories.Common;
+using NetCore.UnitOfWork.Interfaces.Repositories;
+
+namespace NetCore.UnitOfWork.EntityFramework.Repositories
+{
+    public class ChatRepository : GenericRepository<Chat>, IChatRepository
+    {
+        public ChatRepository(AppDbContext context) : base(context) 
+        {
+
+        }
+    }
+}
---
+++ Source/NetCore.UnitOfWork.EntityFramework/Repositories/PersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using NetCore.UnitOfWork.Core.Entities;
+using NetCore.UnitOfWork.EntityFramework.Repositories.Common;
+using NetCore.UnitOfWork.Interfaces.Repositories;
+
+namespace NetCore.UnitOfWork.EntityFramework.Repositories
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
