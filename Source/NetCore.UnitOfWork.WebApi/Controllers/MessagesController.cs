4ac6df8455913c1e15d64a046f9a4bc48348a7c6
Esaf <esaf3349@gmail.com>
2022-10-07 17:56:36 +0300

    Added Persons, Messages controllers

---
+++ Source/NetCore.UnitOfWork.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.UnitOfWork.Core.Entities;
+using NetCore.UnitOfWork.Interfaces;
+using NetCore.UnitOfWork.WebApi.Common.Pagination;
+using System.Threading.Tasks;
+
+namespace NetCore.UnitOfWork.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private IUnitOfWork _uow { get; set; }
+
+        public MessagesController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var message = await _uow.Messages.Get(id);
+
+            return Ok(message);
+        }
+
+        [HttpGet]
+        [Route("/api/chats/{chatId:int}/messages")]
+        public async Task<IActionResult> Get(int chatId, [FromQuery] PageParams page)
+        {
+            var messages = await _uow.Messages.Get(c => c.ChatId == chatId, page.PageNumber, page.PageSize);
+
+            return Ok(messages);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add([FromBody] Message message)
+        {
+            await _uow.Messages.Add(message);
+            await _uow.Commit();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public async Task<IActionResult> Update([FromBody] Message message)
+        {
+            await _uow.Messages.Update(message);
+            await _uow.Commit();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("{id:int}/delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _uow.Messages.Delete(id);
+            await _uow.Commit();
+
+            return Ok();
+        }
+    }
+}
---
+++ Source/NetCore.UnitOfWork.WebApi/Controllers/PersonsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.UnitOfWork.Core.Entities;
+using NetCore.UnitOfWork.Interfaces;
+using NetCore.UnitOfWork.WebApi.Common.Pagination;
+using System.Threading.Tasks;
+
+namespace NetCore.UnitOfWork.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonsController : ControllerBase
+    {
+        private IUnitOfWork _uow { get; set; }
+
+        public PersonsController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var person = await _uow.Persons.Get(id);
+
+            return Ok(person);
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<IActionResult> Get([FromQuery] PageParams page)
+        {
+            var persons = await _uow.Persons.Get(c => true, page.PageNumber, page.PageSize);
+
+            return Ok(persons);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add([FromBody] Person person)
+        {
+            await _uow.Persons.Add(person);
+            await _uow.Commit();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public async Task<IActionResult> Update([FromBody] Person person)
+        {
+            await _uow.Persons.Update(person);
+            await _uow.Commit();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("{id:int}/delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _uow.Persons.Delete(id);
+            await _uow.Commit();
+
+            return Ok();
+        }
+    }
+}
